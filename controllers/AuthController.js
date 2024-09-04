cb51601b489897e190de32ce74daf7bac2637bdf
elissandroa <elissandroa@hotmail.com>
2024-09-04 20:02:05 -0300

    Registrando usuário no sistema / Função Logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -15,9 +15,48 @@ module.exports = class AuthController {
     static async registerPost(req, res) {
         const { name, email, password, confirmPassword } = req.body
         //Password math validation
-        if(password != confirmPassword){
-           req.flash('message', 'As senhas não conferem, tente novamente!')
-           res.render('auth/register')
+        if (password != confirmPassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
         }
+
+        //Check if User exists
+        const checkIfUserExists = await User.findOne({ where: { email: email } })
+
+        if (checkIfUserExists) {
+            req.flash('message', 'O Email já está em uso!')
+            res.render('auth/register')
+            return
+        }
+
+        //Create a password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword,
+        }
+
+        try {
+            const createdUser = await User.create(user)
+            //Initialize Session
+            req.session.userId = createdUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso!')
+
+            req.session.save(() => {
+
+                res.redirect('/')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static logout (req, res) {
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
 router.post('/register', AuthController.registerPost)
+router.get('/logout', AuthController.logout)
 
 
 
